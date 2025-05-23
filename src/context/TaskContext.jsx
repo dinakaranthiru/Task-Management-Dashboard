250475948a02f433c343a73f35d62cbee607598f
dinakaranthiru <dinakarant09@gmail.com>
2025-05-23 10:05:45 +0530

    implement task management context

---
+++ src/context/TaskContext.jsx
@@ -0,0 +1,23 @@
+import { createContext, useContext, useState } from "react";
+
+const TaskContext = createContext();
+
+export const TaskProvider = ({ children }) => {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (task) => setTasks([...tasks, task]);
+
+  const updateTask = (updatedTask) =>
+    setTasks(tasks.map((task) => (task.id === updatedTask.id ? updatedTask : task)));
+
+  const deleteTask = (id) =>
+    setTasks(tasks.filter((task) => task.id !== id));
+
+  return (
+    <TaskContext.Provider value={{ tasks, addTask, updateTask, deleteTask }}>
+      {children}
+    </TaskContext.Provider>
+  );
+};
+
+export const useTasks = () => useContext(TaskContext);
